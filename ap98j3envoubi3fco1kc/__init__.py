32d79a6ab96c525cf12d053101d5c2db4ef5f457
Mohammad Imron Maulana <81271783+imrnmln@users.noreply.github.com>
2024-11-10 13:34:44 +0700

    Update __init__.py

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -440,7 +440,7 @@ subreddits_top_1000 = [
 tor_lock = asyncio.Lock()
 # A flag to track whether the Tor circuit rotation is already in progress
 circuit_rotation_in_progress = False
-TOR_PORTS = [9050, 9052, 9054]
+TOR_PORTS = [9050, 9052, 9054, 9056, 9058]
 
 
 async def load_env_variable(key, default_value=None, none_allowed=False):
