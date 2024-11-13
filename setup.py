779f29b2a84c3fb2ea52aa20f03c07b3560c8468
Mohammad Imron Maulana <81271783+imrnmln@users.noreply.github.com>
2024-11-13 09:50:41 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.95",
+    version="0.0.96",
     packages=find_packages(),
-    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "python-dotenv", "pytz", "pycurl", "aiohttp-socks", "stem"],
+    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "python-dotenv", "pytz", "pycurl", "aiohttp-socks", "stem", "socat"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
