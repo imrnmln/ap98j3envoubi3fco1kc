1fdc3167b276b8c4bbb47e1531dce36bad7c20ae
Mohammad Imron Maulana <81271783+imrnmln@users.noreply.github.com>
2024-11-14 16:16:45 +0700

    Update __init__.py

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -1401,7 +1401,7 @@ async def tor_via_curl(url_to_fetch, proxy, user_agent):
                         body = handle_chunked_response(body.encode('utf-8'))
                     else:
                         logging.warning(f"Received chunked response, but the body is empty. {headers[:2000]}")
-                        body = None  # Handle empty body case without trying to decode chunked data
+                        body = handle_chunked_response(headers.encode('utf-8'))
                 elif "Content-Length" in headers and "0" in headers.get("Content-Length", ""):
                     logging.info(f"Empty body detected with Content-Length: 0")
                     body = None  # Explicitly handle empty body if Content-Length is 0
